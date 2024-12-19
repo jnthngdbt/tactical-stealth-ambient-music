1712465fecb0f2e46961a9a3719054c3fc9c4b1c
Jonathan Godbout <jon.godbout@gmail.com>
2024-12-18 23:20:57 -0500

    Fix subpages build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ export default {
   base: './',  // Use relative paths for assets
   build: {
     outDir: 'docs',  // Use docs/ instead of default dist/ to make it work more easily with Github Pages
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        planning: 'scenes/planning/index.html',
+        binoculars: 'scenes/binoculars/index.html',
+      },
+    },
   },
 };
