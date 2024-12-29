6b92b22e9c6dca47aafda0a9bef1738d7c262214
Jonathan Godbout <jon.godbout@gmail.com>
2024-12-28 22:12:54 -0500

    Add image navigator, convert to tabs

--- binoculars/controls/ControlsManager.ts
+++ binoculars/controls/ControlsManager.ts
@@ -3,167 +3,167 @@ import * as CONST from '../constants.ts';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 
 export class ControlsManager {
-  public window: Window;
-  public camera: THREE.PerspectiveCamera; 
-  public renderer: THREE.WebGLRenderer;
-  public composer: EffectComposer;
-  public planeSize: THREE.Vector2;
-  
-  public isPanning = false;
-  public panSpeedFactor = 10.0;
+	public window: Window;
+	public camera: THREE.PerspectiveCamera; 
+	public renderer: THREE.WebGLRenderer;
+	public composer: EffectComposer;
+	public planeSize: THREE.Vector2;
+	
+	public isPanning = false;
+	public panSpeedFactor = 10.0;
 
-  public minZoomCameraPosZ = 0.8;
+	public minZoomCameraPosZ = 0.8;
 
-  public isSideZooming = false;
-  public sideZoomZoneWidth = 0.15;
-  private sideZoomPos = 0;
+	public isSideZooming = false;
+	public sideZoomZoneWidth = 0.15;
+	private sideZoomPos = 0;
 
-  public scrollZoomSpeedFactor = 0.001;
-  public sideZoomSpeedFactor = 0.002;
+	public scrollZoomSpeedFactor = 0.001;
+	public sideZoomSpeedFactor = 0.002;
 
-  private startMouse = new THREE.Vector2();
-  private startCameraPosition = new THREE.Vector3();
-  
-  constructor(window: Window, 
-    camera: THREE.PerspectiveCamera, 
-    renderer: THREE.WebGLRenderer, 
-    composer: EffectComposer,
-    planeSize: THREE.Vector2) {
+	private startMouse = new THREE.Vector2();
+	private startCameraPosition = new THREE.Vector3();
+	
+	constructor(window: Window, 
+		camera: THREE.PerspectiveCamera, 
+		renderer: THREE.WebGLRenderer, 
+		composer: EffectComposer,
+		planeSize: THREE.Vector2) {
 
-    this.window = window;
-    this.camera = camera;
-    this.renderer = renderer;
-    this.composer = composer;
-    this.planeSize = planeSize;
+		this.window = window;
+		this.camera = camera;
+		this.renderer = renderer;
+		this.composer = composer;
+		this.planeSize = planeSize;
 
-    // Add Event Listeners for Panning
-    window.addEventListener("mousedown", (event) => {
-      this.isSideZoomZone(event.clientX) ? 
-        this.onSideZoomStart(event.clientY) :
-        this.onPanStart(event.clientX, event.clientY);
-    });
+		// Add Event Listeners for Panning
+		window.addEventListener("mousedown", (event) => {
+			this.isSideZoomZone(event.clientX) ? 
+				this.onSideZoomStart(event.clientY) :
+				this.onPanStart(event.clientX, event.clientY);
+		});
 
-    window.addEventListener("mousemove", (event) => {
-      this.isSideZooming ?
-        this.onSideZoom(event.clientY) :
-        this.onPan(event.clientX, event.clientY);
-    });
+		window.addEventListener("mousemove", (event) => {
+			this.isSideZooming ?
+				this.onSideZoom(event.clientY) :
+				this.onPan(event.clientX, event.clientY);
+		});
 
-    window.addEventListener("mouseup", () => {
-      this.isSideZooming ?
-        this.onSideZoomEnd() :
-        this.onPanEnd();
-    });
+		window.addEventListener("mouseup", () => {
+			this.isSideZooming ?
+				this.onSideZoomEnd() :
+				this.onPanEnd();
+		});
 
-    window.addEventListener("touchstart", (event) => {
-      this.isSideZoomZone(event.touches[0].clientX) ? 
-        this.onSideZoomStart(event.touches[0].clientY) :
-        this.onPanStart(event.touches[0].clientX, event.touches[0].clientY);
-    });
+		window.addEventListener("touchstart", (event) => {
+			this.isSideZoomZone(event.touches[0].clientX) ? 
+				this.onSideZoomStart(event.touches[0].clientY) :
+				this.onPanStart(event.touches[0].clientX, event.touches[0].clientY);
+		});
 
-    window.addEventListener("touchmove", (event) => {
-      this.isSideZooming ?
-        this.onSideZoom(event.touches[0].clientY) :
-        this.onPan(event.touches[0].clientX, event.touches[0].clientY);
-    });
+		window.addEventListener("touchmove", (event) => {
+			this.isSideZooming ?
+				this.onSideZoom(event.touches[0].clientY) :
+				this.onPan(event.touches[0].clientX, event.touches[0].clientY);
+		});
 
-    window.addEventListener("touchend", () => {
-      this.isSideZooming ?
-        this.onSideZoomEnd() :
-        this.onPanEnd();
-    });
+		window.addEventListener("touchend", () => {
+			this.isSideZooming ?
+				this.onSideZoomEnd() :
+				this.onPanEnd();
+		});
 
-    // Zoom using scroll wheel
-    window.addEventListener("wheel", (event) => {
-      this.onZoom(event.deltaY * this.scrollZoomSpeedFactor);
-    });
+		// Zoom using scroll wheel
+		window.addEventListener("wheel", (event) => {
+			this.onZoom(event.deltaY * this.scrollZoomSpeedFactor);
+		});
 
-    // Resize Handling
-    window.addEventListener("resize", () => {
-      this.onResize();
-    });
-  }
+		// Resize Handling
+		window.addEventListener("resize", () => {
+			this.onResize();
+		});
+	}
 
-  public update(time: number) {
-    // if needed
-  }
+	public update(time: number) {
+		// if needed
+	}
 
-  private isSideZoomZone(x: number) {
-    return x / this.window.innerWidth > (1.0 - this.sideZoomZoneWidth);
-  }
+	private isSideZoomZone(x: number) {
+		return x / this.window.innerWidth > (1.0 - this.sideZoomZoneWidth);
+	}
 
-  private onSideZoomStart(y: number) {
-    this.isSideZooming = true;
-    this.sideZoomPos = y;
-  }
-  
-  private onSideZoom(y: number) {
-    const deltaY = y - this.sideZoomPos;
-    this.sideZoomPos = y;
-    this.onZoom(deltaY * this.sideZoomSpeedFactor);
-  }
+	private onSideZoomStart(y: number) {
+		this.isSideZooming = true;
+		this.sideZoomPos = y;
+	}
+	
+	private onSideZoom(y: number) {
+		const deltaY = y - this.sideZoomPos;
+		this.sideZoomPos = y;
+		this.onZoom(deltaY * this.sideZoomSpeedFactor);
+	}
 
-  private onSideZoomEnd() {
-    this.isSideZooming = false;
-  }
+	private onSideZoomEnd() {
+		this.isSideZooming = false;
+	}
 
-  private onZoom(delta: number) {
-    this.camera.position.z += delta * this.camera.position.z;
+	private onZoom(delta: number) {
+		this.camera.position.z += delta * this.camera.position.z;
 
-    this.restrictCameraPosition();
-  }
+		this.restrictCameraPosition();
+	}
 
-  private onPanStart(x: number, y: number) {
-    this.isPanning = true;
-    this.startMouse.set(x, y);
-    this.startCameraPosition.copy(this.camera.position);
-  }
+	private onPanStart(x: number, y: number) {
+		this.isPanning = true;
+		this.startMouse.set(x, y);
+		this.startCameraPosition.copy(this.camera.position);
+	}
 
-  private onPanEnd() {
-    this.isPanning = false;
-  }
+	private onPanEnd() {
+		this.isPanning = false;
+	}
 
-  private onPan(x: number, y: number) {
-    if (!this.isPanning) return;
+	private onPan(x: number, y: number) {
+		if (!this.isPanning) return;
 
-    const deltaX = (x - this.startMouse.x) / this.window.innerWidth;
-    const deltaY = (y - this.startMouse.y) / this.window.innerHeight;
+		const deltaX = (x - this.startMouse.x) / this.window.innerWidth;
+		const deltaY = (y - this.startMouse.y) / this.window.innerHeight;
 
-    this.camera.position.x = this.startCameraPosition.x + deltaX * this.panSpeedFactor;
-    this.camera.position.y = this.startCameraPosition.y - deltaY * this.panSpeedFactor;
+		this.camera.position.x = this.startCameraPosition.x + deltaX * this.panSpeedFactor;
+		this.camera.position.y = this.startCameraPosition.y - deltaY * this.panSpeedFactor;
 
-    this.restrictCameraPosition();
-  }
+		this.restrictCameraPosition();
+	}
 
-  private onResize() {
-    this.camera.aspect = this.window.innerWidth / this.window.innerHeight;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(this.window.innerWidth, this.window.innerHeight);
-    this.composer.setSize(this.window.innerWidth, this.window.innerHeight);
-    this.restrictCameraPosition();
-  }
+	private onResize() {
+		this.camera.aspect = this.window.innerWidth / this.window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+		this.composer.setSize(this.window.innerWidth, this.window.innerHeight);
+		this.restrictCameraPosition();
+	}
 
-  private restrictCameraPosition() {
-    const edgeFactor = 0.85; // account for black border of binoculars
-    const z = this.camera.position.z;
-    const fov = (edgeFactor * this.camera.fov / 2) * (Math.PI / 180);
-    const cameraAr = this.camera.aspect;
+	private restrictCameraPosition() {
+		const edgeFactor = 0.85; // account for black border of binoculars
+		const z = this.camera.position.z;
+		const fov = (edgeFactor * this.camera.fov / 2) * (Math.PI / 180);
+		const cameraAr = this.camera.aspect;
 
-    // Calculate frustum dimensions at the plane's distance
-    const frustumHeight = 2 * z * Math.tan(fov);
-    const frustumWidth = frustumHeight * cameraAr;
+		// Calculate frustum dimensions at the plane's distance
+		const frustumHeight = 2 * z * Math.tan(fov);
+		const frustumWidth = frustumHeight * cameraAr;
 
-    // Calculate camera boundaries
-    const xMax = (this.planeSize.x - frustumWidth) / 2;
-    const yMax = (this.planeSize.y - frustumHeight) / 2;
+		// Calculate camera boundaries
+		const xMax = (this.planeSize.x - frustumWidth) / 2;
+		const yMax = (this.planeSize.y - frustumHeight) / 2;
 
-    // Restrict zoom camera position
-    const zMaxFromHeight = this.planeSize.y / (2 * Math.tan(fov));
-    const zMaxFromWidth = this.planeSize.x / (2 * Math.tan(fov) * cameraAr);
-    const zMax = Math.min(zMaxFromHeight, zMaxFromWidth)
+		// Restrict zoom camera position
+		const zMaxFromHeight = this.planeSize.y / (2 * Math.tan(fov));
+		const zMaxFromWidth = this.planeSize.x / (2 * Math.tan(fov) * cameraAr);
+		const zMax = Math.min(zMaxFromHeight, zMaxFromWidth)
 
-    this.camera.position.x = THREE.MathUtils.clamp(this.camera.position.x, -xMax, xMax);
-    this.camera.position.y = THREE.MathUtils.clamp(this.camera.position.y, -yMax, yMax);
-    this.camera.position.z = THREE.MathUtils.clamp(this.camera.position.z, this.minZoomCameraPosZ, zMax);
-  }
+		this.camera.position.x = THREE.MathUtils.clamp(this.camera.position.x, -xMax, xMax);
+		this.camera.position.y = THREE.MathUtils.clamp(this.camera.position.y, -yMax, yMax);
+		this.camera.position.z = THREE.MathUtils.clamp(this.camera.position.z, this.minZoomCameraPosZ, zMax);
+	}
 }
--- binoculars/main.ts
+++ binoculars/main.ts
@@ -14,41 +14,68 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// ================================
+// #region SCENE IMAGE
+// ================================
+
 // Some images for testing:
-// const img = "jnthngdbt_Dark_theme_heavily_defended_concrete_multi-building_b_2e4dae5c-a670-4c21-a6c0-abe8f0757a25.png"; // (big sad complex)
-// const img = "jnthngdbt_Dark_theme_heavily_defended_concrete_multi-building_b_f081fe34-33ad-4b6b-bf7d-63a0b79bb8b8.png"; // (dam towers)
-// const img = "jnthngdbt_Dark_theme_heavily_fortified_concrete_multi-building__71c05842-c354-4d31-bd7c-fc93629f3f38.png"; // (cold dam base)
-// const img = "jnthngdbt_Heavily_defended_desert_concrete_multi-building_bunke_4e449e81-c3bd-4d08-94ec-aa464fb40833.png"; // (daylight 2 silos)
-// const img = "jnthngdbt_Heavily_fortified_and_defended_desert_concrete_multi-_0a2ed1db-e1f4-4419-8dbf-ce26f915394e.png"; // (trapezoid)
-// const img = "jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_74d166ca-8493-4f48-a146-64ca25121cab.png"; // (blue house)
-// const img = "jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_92a6a60d-4ee6-40bd-82ed-1df5387ec3a8.png"; // (spatioport)
-// const img = "jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_354daa91-edcb-4e11-a44f-f56a098819f8.png"; // (nice gray base)
-// const img = "jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_0701d598-e389-4b2a-a124-7589788e4d04.png"; // (orange units)
-const img = "jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_ce184244-4427-4403-9573-914a55257101.png"; // (cozy silos)
-// const img = "jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_6cc95bcb-8818-4a4b-882c-8f8367139ad8.png"; // (badass waterside)
-// const img = "jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_cdcc27e2-b964-4ba4-9990-92acce687e7c.png"; // (red industry)
-// const img = "jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_cf4c2416-c763-45a1-8ef7-d1381616799a.png"; // (sea port)
-// const img = "jnthngdbt_Midnight_heavily_defended_desert_concrete_multi-build_4a24248c-3833-4961-bb7f-372992a6cb87.png"; // (original structure)
-// const img = "jnthngdbt_Midnight_heavily_defended_desert_concrete_multi-build_f0371757-ae87-48d0-b7c5-856f2e4edb59.png"; // (moonlike)
+const images = [
+	"jnthngdbt_Dark_theme_heavily_defended_concrete_multi-building_b_2e4dae5c-a670-4c21-a6c0-abe8f0757a25.png", // (big sad complex)
+	"jnthngdbt_Dark_theme_heavily_defended_concrete_multi-building_b_f081fe34-33ad-4b6b-bf7d-63a0b79bb8b8.png", // (dam towers)
+	"jnthngdbt_Dark_theme_heavily_fortified_concrete_multi-building__71c05842-c354-4d31-bd7c-fc93629f3f38.png", // (cold dam base)
+	"jnthngdbt_Heavily_defended_desert_concrete_multi-building_bunke_4e449e81-c3bd-4d08-94ec-aa464fb40833.png", // (daylight 2 silos)
+	"jnthngdbt_Heavily_fortified_and_defended_desert_concrete_multi-_0a2ed1db-e1f4-4419-8dbf-ce26f915394e.png", // (trapezoid)
+	"jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_74d166ca-8493-4f48-a146-64ca25121cab.png", // (blue house)
+	"jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_92a6a60d-4ee6-40bd-82ed-1df5387ec3a8.png", // (spatioport)
+	"jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_354daa91-edcb-4e11-a44f-f56a098819f8.png", // (nice gray base)
+	"jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_0701d598-e389-4b2a-a124-7589788e4d04.png", // (orange units)
+	"jnthngdbt_Midnight_cyberpunk_concrete_multi-building_bunker_com_ce184244-4427-4403-9573-914a55257101.png", // (cozy silos)
+	"jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_6cc95bcb-8818-4a4b-882c-8f8367139ad8.png", // (badass waterside)
+	"jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_cdcc27e2-b964-4ba4-9990-92acce687e7c.png", // (red industry)
+	"jnthngdbt_Midnight_futuristic_concrete_multi-building_bunker_co_cf4c2416-c763-45a1-8ef7-d1381616799a.png", // (sea port)
+	"jnthngdbt_Midnight_heavily_defended_desert_concrete_multi-build_4a24248c-3833-4961-bb7f-372992a6cb87.png", // (original structure)
+	"jnthngdbt_Midnight_heavily_defended_desert_concrete_multi-build_f0371757-ae87-48d0-b7c5-856f2e4edb59.png", // (moonlike)
+];
 
 const host = "https://raw.githubusercontent.com/jnthngdbt/tactical-steath-ambient-music-assets/refs/heads/main/binoculars/high-res/";
 
 var planeSize = new THREE.Vector2(0, 0);
+var planeMesh: THREE.Mesh;
 
-// Load an Image Texture
 const textureLoader = new THREE.TextureLoader();
-textureLoader.load(host + img, (texture) => {
-  PixelatedNoiseMaterial.uniforms.diffuseTexture.value = texture;
-  PixelatedNoiseMaterial.uniforms.resolution.value.set(texture.image.width, texture.image.height);
 
-  // Create a plane geometry and material
-  const aspectRatio = texture.image.width / texture.image.height;
-  planeSize.set(10, 10 / aspectRatio);
-  const geometry = new THREE.PlaneGeometry(planeSize.x, planeSize.y);
-  const plane = new THREE.Mesh(geometry, PixelatedNoiseMaterial);
+function loadImage(idx: number) {
+	if (idx < 0) imageIndex = images.length - 1;
+	else if (idx >= images.length) imageIndex = 0;
+	else imageIndex = idx;
 
-  scene.add(plane);
-});
+	const img = images[imageIndex];
+
+	textureLoader.load(host + img, (texture) => {
+		PixelatedNoiseMaterial.uniforms.diffuseTexture.value = texture;
+		PixelatedNoiseMaterial.uniforms.resolution.value.set(texture.image.width, texture.image.height);
+	
+		// Create a plane geometry and material
+		const aspectRatio = texture.image.width / texture.image.height;
+		planeSize.set(10, 10 / aspectRatio);
+		const geometry = new THREE.PlaneGeometry(planeSize.x, planeSize.y);
+	
+		scene.remove(planeMesh);
+		planeMesh = new THREE.Mesh(geometry, PixelatedNoiseMaterial);
+		scene.add(planeMesh);
+	});
+}
+
+// Load the first image
+var imageIndex = 3;
+loadImage(imageIndex);
+
+document.getElementById('prevImageBtn')?.addEventListener('click', () => loadImage(imageIndex - 1));
+document.getElementById('nextImageBtn')?.addEventListener('click', () => loadImage(imageIndex + 1));
+
+// ================================
+// #region POST-PROCESSING
+// ================================
 
 // Post-Processing Setup
 const composer = new EffectComposer(renderer);
@@ -56,27 +83,35 @@ composer.addPass(new RenderPass(scene, camera));
 
 // Add post-processing passes... (if necessary)
 
+// ================================
+// #region CONTROLS
+// ================================
+
 // Pan and zoom controls
 var navControls = new ControlsManager(window, camera, renderer, composer, planeSize);
 
 // Camera jitter for a shaky effect
 var cameraJitter = new CameraJitter(camera, 0.0015, 2);
 
+// ================================
+// #region ANIMATION
+// ================================
+
 // Clock for time-based animation
 const clock = new THREE.Clock();
 
 // Animation Loop
 function animate() {
-  const time = clock.getElapsedTime();
+	const time = clock.getElapsedTime();
 
-  PixelatedNoiseMaterial.uniforms.time.value = time;
+	PixelatedNoiseMaterial.uniforms.time.value = time;
 
-  navControls.update(time);
-  cameraJitter.update(time);
+	navControls.update(time);
+	cameraJitter.update(time);
 
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  composer.render();
+	composer.render();
 }
 
 animate();
